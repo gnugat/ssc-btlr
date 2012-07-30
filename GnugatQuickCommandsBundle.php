904481eb3c478e15dbe4975783e37fef53a25854
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-07-30 19:42:43 +0100

    [Config] Creating the bundle class

---
+++ GnugatQuickCommandsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Gnugat\QuickCommandsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle class.
+ *
+ * @author Loic chardonnet <loic.chardonnet@gmail.com>
+ */
+class GnugatQuickCommandsBundle extends Bundle
+{
+}
